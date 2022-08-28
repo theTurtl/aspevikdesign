536fefc1f14995416986c6165366a79ff543992e
theTurtl <addimanni@gmail.com>
2022-08-28 17:06:16 +0200

    Added a few .svg, one being logo. Started on 1st part of homepage

--- src/App.js
+++ src/App.js
@@ -5,6 +5,31 @@ function App() {
     <div className="App">
       <header className="App-header">
       </header>
+      <div className='introSection'>
+        <div className='navBar'>
+          <div className='leftContainer'>
+            <div className='logo'></div>
+            <div className='statusBar'>Hyggelig å treffe deg! :)</div>
+          </div>
+          <div className='rightContainer'>
+            <div className='biggerProjects'>
+              <p>Større prosjekter</p>
+              <div className='arriveSoon'>Kommer!</div>
+            </div>
+            <div className='contactBut'>Kontakt</div>
+            <div className='languageSwitcher'>
+              <p>NO</p>
+              <div className='downArrow'></div>
+            </div>
+          </div>
+        </div>
+        <div className='animationBanner'>
+          <div className='animationContainer'></div>
+          <div className='bannerToggle'>
+          </div>
+        </div>
+        
+      </div>
     </div>
   );
 }
